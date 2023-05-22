599f2757a96229e8395936e6e80bad4fbc7132f1
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-22 19:34:54 -0400

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if(distance <= 42){
+        return 42 - distance
+    }
+    else if(distance >= 42){
+        return distance - 42
+    }
+}
+
+
+function distanceFromHqInFeet(startingStreet){
+    let blocks = distanceFromHqInBlocks(startingStreet)
+    return blocks * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(destination < start){
+        return (start - destination) * 264
+    }
+    else if(destination > start){
+        return (destination - start) * 264
+    }
+  
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+    if(distance <= 400){
+        return 0
+    }
+    else if(distance <= 2000){
+        let fare = (distance - 400) * 0.02
+        return fare 
+    } 
+    else if(distance <= 2500){
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
